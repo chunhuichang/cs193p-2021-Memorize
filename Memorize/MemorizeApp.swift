3592bab4cd83a0be142eaa8ca3d7cfa20b603b98
Chun Hui Chang <jill20021@gmail.com>
2022-03-03 11:23:44 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jill Chang on 2022/3/3.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
