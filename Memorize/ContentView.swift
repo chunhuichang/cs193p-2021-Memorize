808992a452f71f3c831270698dafcc4ab41c22bd
Chun Hui Chang <jill20021@gmail.com>
2022-03-04 15:26:02 +0800

    feat: valid add and remove button
    replace text to image icon

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -17,34 +17,36 @@ struct ContentView: View {
                     CardView(content: emoji)
                 }
             }
+            .foregroundColor(.red)
+            Spacer()
             HStack {
                 remove
                 Spacer()
                 add
             }
+            .font(.largeTitle)
             .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
     }
     
     var add: some View {
         Button {
-            emojiCount += 1
-        } label: {
-            VStack{
-                Text("Add Card")
+            if emojiCount < emojis.count {
+                emojiCount += 1
             }
+        } label: {
+            Image(systemName: "plus.circle")
         }
     }
     
     var remove: some View {
         Button {
-            emojiCount -= 1
-        } label: {
-            VStack{
-                Text("Remove Card")
+            if emojiCount > 1 {
+                emojiCount -= 1
             }
+        } label: {
+            Image(systemName: "minus.circle")
         }
     }
 }
