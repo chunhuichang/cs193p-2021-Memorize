ec686dbe5259be0441b98ebdd5593e647b413b58
Chun Hui Chang <jill20021@gmail.com>
2022-03-22 17:56:12 +0800

    refactor: set card disappear in AspectVGrid content
    @ViewBuilder

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -12,11 +12,15 @@ struct EmojiMemoryGameView: View {
     
     var body: some View {
         AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
-            CardView(card: card)
-                .padding(4)
-                .onTapGesture {
-                    game.choose(card)
-                }
+            if card.isMatched, !card.isFaceUp {
+                Rectangle().opacity(0)
+            } else {
+                CardView(card: card)
+                    .padding(4)
+                    .onTapGesture {
+                        game.choose(card)
+                    }
+            }
         }
         .foregroundColor(.red)
         .padding(.horizontal)
@@ -35,8 +39,6 @@ struct CardView: View {
                     
                     Text(card.content)
                         .font(font(in: geometry.size))
-                } else if card.isMatched {
-                    shape.opacity(0)
                 } else {
                     shape.fill()
                 }
